90c301080f9efd7751e9a05b034fc3b6244e3e6a
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2022-02-18 17:03:24 +0100

    rename/reorder topoplot

--- src/UnfoldMakie.jl
+++ src/UnfoldMakie.jl
@@ -7,12 +7,14 @@ using Unfold
 
 include("plot_design.jl")
 include("plot_results.jl")
-include("topoplot.jl")
+include("plot_topoplot.jl")
 export plot_results
 export plot
 export topoplot
 export topoplot!
 export Topoplot
+export plot_topoplot
+export plot_topoplot_series
 # Write your package code here.
 
 end
