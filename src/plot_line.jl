0863b4068765faf4bd905b18d6b207c4c723c0b6
SoerenDoering <doering.soeren@gmx.de>
2022-08-26 12:57:33 +0200

    plot_line cleanup

--- src/plot_line.jl
+++ src/plot_line.jl
@@ -45,15 +45,13 @@ function plot_line!(f::Union{GridPosition, Figure}, plotData::DataFrame, config:
     plotData = deepcopy(plotData)
 
     config.resolveMappings(plotData)
-    
-    # @show names(plotData)
-    # if isnothing(config.mappingData.y)
-    #     config.mappingData.y = "estimate" ∈  names(plotData) ? :estimate : "yhat" ∈  names(plotData) ? :yhat : @error("please specify y-axis")
-    # end
+
+    # turn "nothing" from group columns into :fixef
     if "group" ∈  names(plotData)
         plotData.group = plotData.group .|> a -> isnothing(a) ? :fixef : a
     end
-    
+
+    # check if stderror values exist and create new collumsn with high and low band
     if "stderror" ∈  names(plotData) && config.extraData.stderror
         plotData.stderror = plotData.stderror .|> a -> isnothing(a) ? 0. : a
         plotData[!,:se_low]  = plotData[:,config.mappingData.y] .- plotData.stderror
@@ -63,21 +61,16 @@ function plot_line!(f::Union{GridPosition, Figure}, plotData::DataFrame, config:
     # Get topocolors if topoPlot Legend active
     if (config.extraData.topoLegend) 
         allPositions, colors = getTopoColor(plotData, config)
-    end
-
-    # return allPositions
-    # Categorical mapping
-    # convert color column into string, so no wrong grouping happens
-    if config.extraData.categoricalColor && (:color ∈ keys(config.mappingData))
-        # if color is used, use upper line
-        # plotData[!, config.mappingData.color] = plotData[!, config.mappingData.color] .|> c -> string(c)
-        config.mappingData = merge(config.mappingData,(;color=config.mappingData.color=>nonnumeric))
-    end
-    # converts group column into string
-    if config.extraData.categoricalGroup && (:group ∈ keys(config.mappingData))
-        # if color is used, use upper line
-        # plotData[!, config.mappingData.group] = plotData[!, config.mappingData.group] .|> c -> string(c)
-        config.mappingData = merge(config.mappingData,(;group=config.mappingData.group=>nonnumeric))
+    else
+        # Categorical mapping
+        # convert color column into string, so no wrong grouping happens
+        if config.extraData.categoricalColor && (:color ∈ keys(config.mappingData))
+            config.mappingData = merge(config.mappingData,(;color=config.mappingData.color=>nonnumeric))
+        end
+        # converts group column into string
+        if config.extraData.categoricalGroup && (:group ∈ keys(config.mappingData))
+            config.mappingData = merge(config.mappingData,(;group=config.mappingData.group=>nonnumeric))
+        end
     end
 
     # pValues will break if x and y are included in this step
@@ -86,6 +79,8 @@ function plot_line!(f::Union{GridPosition, Figure}, plotData::DataFrame, config:
     xy_mapp = mapping(config.mappingData.x, config.mappingData.y)
 
     basic = visual(Lines; config.visualData...) * xy_mapp
+
+    # add band of sdterrors
     if config.extraData.stderror
         m_se = mapping(config.mappingData.x,:se_low,:se_high)
         basic = basic + visual(Band,alpha=0.5)*m_se
@@ -99,8 +94,7 @@ function plot_line!(f::Union{GridPosition, Figure}, plotData::DataFrame, config:
     end
     
     plotEquation = basic * mapp
-    # return plotEquation
-    
+
     # add topoLegend if topoPlot Legend active
     if (config.extraData.topoLegend)    
         topoplotLegend(f, allPositions)
@@ -108,8 +102,7 @@ function plot_line!(f::Union{GridPosition, Figure}, plotData::DataFrame, config:
     else
         drawing = draw!(f[1,1],plotEquation)
     end
-    # if palettes=(color=colors,), nonnumeric columns crash program
-    # drawing = draw!(f[1,1],plotEquation; colormap=:grays)
+
     
     applyLayoutSettings(config; fig = f, drawing = drawing)
 
@@ -117,118 +110,6 @@ function plot_line!(f::Union{GridPosition, Figure}, plotData::DataFrame, config:
     
 end
 
-
-function plot_results(results::DataFrame;y=nothing,
-    color=:coefname,
-    col=:basisname,
-    row=:group,
-    stderror=false,
-    pvalue = DataFrame(:from=>[],:to=>[]),
-    kwargs...)
-
-    results = deepcopy(results)
-
-    @assert "time" ∈ names(results) ":time has to be a column of the input dataframe"
-
-    if isnothing(y)
-        y = "estimate" ∈  names(results) ? :estimate : "yhat" ∈  names(results) ? :yhat : @error("please specify y-axis")
-    end
-    
-    # replace missing/nothing values
-    if "group" ∈  names(results)
-        results.group = results.group .|> a -> isnothing(a) ? :fixef : a
-    end
-    if "stderror" ∈  names(results) && stderror
-        results.stderror = results.stderror .|> a -> isnothing(a) ? 0. : a
-        results[!,:se_low]  = results[:,y] .- results.stderror
-        results[!,:se_high] = results[:,y] .+ results.stderror
-    end
-    
-    # check if mappings exist
-    
-    if string(color) ∉ names(results)
-        color != :coefname ? @error("user specified color=$color not found in DataFrame") : nothing
-        color = 1
-    end
-    if string(col) ∉ names(results)
-        col != :basisname ? @error("user specified col=$col not found in DataFrame") : nothing
-        col = 1
-    end
-    if string(row) ∉ names(results)
-        row != :group ? error("user specified row=$row not found in DataFrame") : nothing
-        row = 1
-    end
-
-    m = mapping(color=color,col=col,row=row;kwargs...)
-
-    m_li = mapping(:time,y)
-
-    basic =  visual(Lines) * m_li
-    
-    if stderror
-        m_se = mapping(:time,:se_low,:se_high)
-        basic = basic + visual(Band,alpha=0.5)*m_se
-    end
-
-    basic = basic*data(results)
-       
-    
-    
-
-    # add the pvalues
-    if !isempty(pvalue)
-        p = deepcopy(pvalue)
-
-        # for now, add them to the fixed effect
-        if "group" ∉  names(p)
-            # group not specified using first
-            if "group" ∈  names(results)
-                p[!,:group] .= results[1,:group]
-                if length(unique(results.group))>1
-                    @warn "multiple groups found, choosing first one"
-                end
-            else
-                p[!,:group] .= 1
-            end
-        end
-        
-
-        # rename to match the res-dataframe
-
-        shouldHave = hcat(col,row,color)
-        shouldHave = shouldHave[shouldHave.!=1] # remove defaults as defined above
-        if ~isempty(kwargs)
-             shouldHave = hcat(shouldHave,(values(kwargs)))
-        end
-        shouldHave = string.(shouldHave)
-        
-        for k in shouldHave
-            if k ∉ names(p)
-                p[!,k] .= results[1,k]
-            end
-
-        end
-
-        @show shouldHave
-        un = unique(p[!,color])
-        # define an index to dodge the lines vertically
-        p[!,:sigindex] .=  [findfirst(un .== x) for x in p.coefname]
-        
-        scaleY = [minimum(results.estimate),maximum(results.estimate)]
-        stepY = scaleY[2]-scaleY[1]
-        posY = stepY*-0.05+scaleY[1]
-        Δt = diff(results.time[1:2])[1]
-        Δy = 0.01
-        p[!,:segments] = [Makie.Rect(Makie.Vec(x,posY+stepY*(Δy*(n-1))),Makie.Vec(y-x+Δt,0.5*Δy*stepY)) for (x,y,n) in zip(p.from,p.to,p.sigindex)]
-        basic =  basic + (data(p)*mapping(:segments)*visual(Poly))
-    end
-
-    # draw it!
-
-    d = basic*m|> draw
-    return d
-    
-end
 
 function eegHeadMatrix(positions, center, radius)
     oldCenter = mean(positions)
