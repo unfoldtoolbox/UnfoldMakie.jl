dc610fc9e54cd45322e9fdac7ce631bd3d346ab7
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2022-11-30 14:09:54 +0100

    test fixes

--- test/test_plot_circulareegtopoplot.jl
+++ test/test_plot_circulareegtopoplot.jl
@@ -6,7 +6,7 @@
             predictor = [70,80,400],
             positions = [Point(1.0,2.0), Point(1.0,2.0), Point(1.0,2.0)],
         )
-
+        outOfBoundsErr = nothing
         try
             plot_circulareegtopoplot(testdf)
         catch outOfBoundsErr
@@ -17,13 +17,13 @@
     end
 
     @testset "tooManyBoundsErr" begin
-
         testdf = DataFrame(
             effect = [[4.0,1.0],[4.0,3.0],[4.0,3.0]],
             predictor = [70,80,90],
             positions = [Point(1.0,2.0), Point(1.0,2.0), Point(1.0,2.0)],
-        )
-
+            )
+            
+        tooManyBoundsErr = nothing
         try
             config = PlotConfig(:circeegtopo).setExtraValues!(predictorBounds = [0, 100, 360])
             plot_circulareegtopoplot(testdf, config)
@@ -39,20 +39,22 @@ end
 
 @testset "testing calculateGlobalMaxValues" begin
     # notice: this function uses the 0.01 and the 0.99 quantile
-    @test calculateGlobalMaxValues([[1, 2], [3, 4], [5, 6, 7], [8], [9, 10]]) == (-9.99, 9.99)
-    @test calculateGlobalMaxValues([[-1, -2], [-3, -4], [5, 6, 7], [-8], [9, 10]]) == (-9.99, 9.99)
+    pred =repeat(1:3,inner=5)
+    val = [1:5...; 6:10...; 11:15...]
+    @test UnfoldMakie.calculateGlobalMaxValues(val,pred) == (-14.96, 14.96)
+    
 end
 
 @testset "testing calculateAxisLabels" begin
     # notice: this function uses the 0.01 and the 0.99 quantile
-    @test calculateAxisLabels([0, 360]) == ["0", "90", "180   ", "270"]
-    @test calculateAxisLabels([-180, 180]) == ["-180", "-90", "0   ", "90"]
-    @test calculateAxisLabels([0, 100]) == ["0", "25", "50   ", "75"]
+    @test UnfoldMakie.calculateAxisLabels([0, 360]) == ["0", "90", "180   ", "270"]
+    @test UnfoldMakie.calculateAxisLabels([-180, 180]) == ["-180", "-90", "0   ", "90"]
+    @test UnfoldMakie.calculateAxisLabels([0, 100]) == ["0", "25", "50   ", "75"]
 end
 
 @testset "testing calculateBBox" begin
-    @test calculateBBox([0, 0], [1000, 1000], 180, [0, 360]) == BBox(50.0, 250.0, 400.0, 600.0)
-    @test calculateBBox([0, 0], [1000, 1000], -45, [0, 360]) == BBox(647.48737, 847.48737, 152.51262, 352.51262)
-    @test calculateBBox([0, 0], [1000, 1000], -180, [-180, 180]) == BBox(750.0, 950.0, 400.0, 600.0)
+    @test UnfoldMakie.calculateBBox([0, 0], [1000, 1000], 180, [0, 360]) == BBox(50.0, 250.0, 400.0, 600.0)
+    @test UnfoldMakie.calculateBBox([0, 0], [1000, 1000], -45, [0, 360]) == BBox(647.48737, 847.48737, 152.51262, 352.51262)
+    @test UnfoldMakie.calculateBBox([0, 0], [1000, 1000], -180, [-180, 180]) == BBox(750.0, 950.0, 400.0, 600.0)
 end
 
