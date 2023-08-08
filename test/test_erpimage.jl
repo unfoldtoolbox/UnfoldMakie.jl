1ce22d596e361b7666e2f84245c4114641074aa2
Vladimir Mikheev <dezem@bk.ru>
2023-08-08 14:50:20 +0000

    erpimage docfile update

--- test/test_erpimage.jl
+++ test/test_erpimage.jl
@@ -1,10 +1,15 @@
-include("setup.jl")
-@testset "testing calculateBBox" begin
+#include("setup.jl")
+@testset "testing no bottom erp plot" begin
     data, evts = UnfoldSim.predef_eeg(;noiselevel=10, return_epoched=true)
     plot_erpimage(data; ploterp = false,)
 end
 
-@testset "testing calculateBBox" begin
+@testset "testing with bottom erp plot" begin
     data, evts = UnfoldSim.predef_eeg(;noiselevel=10, return_epoched=true)
     plot_erpimage(data; ploterp = true,)
+end
+
+@testset "testing no bottom erp plot in extra mode" begin
+    data, evts = UnfoldSim.predef_eeg(;noiselevel=10, return_epoched=true)
+    plot_erpimage(data; extra = (ploterp = true,),)
 end
