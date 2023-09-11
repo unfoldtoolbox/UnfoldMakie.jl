bc63357d0d5426037397531fba1d43d671225654
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2023-09-11 16:44:59 +0000

    fixes #66,#67,#64,#63

--- src/plot_topoplotseries.jl
+++ src/plot_topoplotseries.jl
@@ -13,7 +13,8 @@ Plot a Topoplot Series.
 
 ## Extra Data Behavior (...;extra=(;[key]=value)):
 `combinefun` (default `mean`) can be used to specify how the samples within `Δbin` are combined.
-
+`bin_labels` (default `true`) - plot the time-window bin size as xlabels at the last row of the plot
+`rasterize_heatmaps` (deault `true`) - when saving a svg - enforce rasterization of the plot heatmap. This has the benefit that all lines/points are vectors, except the interpolated heatmap. This is typically what you want, because else you get ~500x500 vectors per topoplot, which makes everything super slow...
 
 
 ## Return Value:
@@ -39,16 +40,24 @@ function plot_topoplotseries!(f::Union{GridPosition,Figure}, plotData::DataFrame
         plotData.label = plotData.channel
     end
 
-    eeg_topoplot_series!(f, plotData, Δbin;
+    ftopo = eeg_topoplot_series!(f, plotData, Δbin;
         col_y=config.mapping.y,
         col_label=:label,
         col=config.mapping.col,
         row=config.mapping.row,
+        bin_labels = config.extra.bin_labels,
+        rasterize_heatmap = config.extra.rasterize_heatmap,
         combinefun=config.extra.combinefun,
         positions=positions,
         config.visual...
     )
 
+    if config.layout.showLegend
+        @show "leegeeend"
+        d = ftopo.content[1].scene.plots[1]
+        
+        Colorbar(f[1,end+1],colormap=d.colormap,colorrange=d.colorrange,height=100)
+    end
     return f
 
 end
