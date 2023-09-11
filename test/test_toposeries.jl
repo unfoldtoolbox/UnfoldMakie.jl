82bf4dc556455772f903832efe9ab2b4b469413f
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2023-09-11 16:55:38 +0000

    removed the double script example_data, loaded the docs one

--- test/test_toposeries.jl
+++ test/test_toposeries.jl
@@ -1,5 +1,4 @@
-# include("../UnfoldMakie/test/test_toposeries.jl")
-#include("setup.jl")
+
 @testset "testing calculateBBox" begin
     data, positions = TopoPlots.example_data()
     df = UnfoldMakie.eeg_matrix_to_dataframe(data[:, :, 1], string.(1:length(positions)));
