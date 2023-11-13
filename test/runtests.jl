fafc2ef4793ece827181b968b09033eceb4c4639
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2023-11-13 18:30:35 +0000

    invalid kwargs specification should throw an error now

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,26 +2,30 @@ using UnfoldMakie
 include("setup.jl")
 #include("../src/UnfoldMakie.jl")
 
-@testset "UnfoldMakie.jl" begin
+
+@testset "Test Config" begin
+    include("test_config.jl")
+end
+@testset "CircularEEGTopoPlot" begin
     include("test_plot_circulareegtopoplot.jl")
 end
 
-@testset "UnfoldMakie.jl" begin
+@testset "TopoSeries" begin
     include("test_toposeries.jl")
 end
 
-@testset "UnfoldMakie.jl" begin
+@testset "ERPImage" begin
     include("test_erpimage.jl")
 end
 
-@testset "UnfoldMakie.jl" begin
+@testset "TopoPlot" begin
     include("test_topoplot.jl")
 end
 
-@testset "UnfoldMakie.jl" begin
+@testset "Butterfly" begin
     include("test_butterfly.jl")
 end
 
-@testset "UnfoldMakie.jl" begin
+@testset "AllPlots" begin
     include("test_all.jl")
 end
