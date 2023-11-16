e5e701723246063a1b5ce80526ae92ee4785a802
Vladimir Mikheev <dezem@bk.ru>
2023-11-16 14:55:50 +0000

    formatting

--- docs/example_data.jl
+++ docs/example_data.jl
@@ -7,60 +7,96 @@ type ==
 "TopoPlots.jl"(default) Returns tidy DataFrame with two conditions, 64 channels and 800ms of data. This is an average over many subjects.
 """
 
-function example_data(example="TopoPlots.jl")
+function example_data(example = "TopoPlots.jl")
 
-	if example == "UnfoldLinearModel"
-		# load and generate a simulated Unfold Design
-		data,evts = UnfoldSim.predef_eeg(;noiselevel=10,return_epoched=true)
-		data = reshape(data,1,size(data)...)
-		f  = @formula 0~1+condition+continuous
-		# generate ModelStruct
-		se_solver =(x,y)->Unfold.solver_default(x,y,stderror=true)
-		return fit(UnfoldModel,(Dict(Any=>(f,range(0,length=size(data,2),step=1/100)))) ,evts,data;solver=se_solver)
-	elseif example == "UnfoldLinearModelMultiChannel"
-			# load and generate a simulated Unfold Design
-			cAll = DataFrame()
-			sfreq = 100
-			for ch = 1:5
-			data,evts = UnfoldSim.predef_eeg(;p1 = (p100(;sfreq=sfreq), @formula(0~1),[5],Dict()), 
-			
-			n1 = (n170(;sfreq=sfreq), @formula(0~1+condition),[5,-ch*0.5],Dict()), 
-			 p3 = (p300(;sfreq=sfreq), @formula(0~1+continuous),[ch,1],Dict()), 
-			return_epoched=true)
-			data = reshape(data,1,size(data)...)
-			f  = @formula 0~1+condition+continuous
-			# generate ModelStruct
-		
-			m= fit(UnfoldModel,(Dict(Any=>(f,range(0,length=size(data,2),step=1/100)))) ,evts,data)
-			d = coeftable(m)
-			d.channel .= ch
-			cAll = append!(cAll,d)
-			end
-			return cAll
+    if example == "UnfoldLinearModel"
+        # load and generate a simulated Unfold Design
+        data, evts = UnfoldSim.predef_eeg(; noiselevel = 10, return_epoched = true)
+        data = reshape(data, 1, size(data)...)
+        f = @formula 0 ~ 1 + condition + continuous
+        # generate ModelStruct
+        se_solver = (x, y) -> Unfold.solver_default(x, y, stderror = true)
+        return fit(
+            UnfoldModel,
+            (Dict(Any => (f, range(0, length = size(data, 2), step = 1 / 100)))),
+            evts,
+            data;
+            solver = se_solver,
+        )
+    elseif example == "UnfoldLinearModelMultiChannel"
+        # load and generate a simulated Unfold Design
+        cAll = DataFrame()
+        sfreq = 100
+        for ch = 1:5
+            data, evts = UnfoldSim.predef_eeg(;
+                p1 = (p100(; sfreq = sfreq), @formula(0 ~ 1), [5], Dict()),
+                n1 = (
+                    n170(; sfreq = sfreq),
+                    @formula(0 ~ 1 + condition),
+                    [5, -ch * 0.5],
+                    Dict(),
+                ),
+                p3 = (p300(; sfreq = sfreq), @formula(0 ~ 1 + continuous), [ch, 1], Dict()),
+                return_epoched = true,
+            )
+            data = reshape(data, 1, size(data)...)
+            f = @formula 0 ~ 1 + condition + continuous
+            # generate ModelStruct
 
-	elseif example == "UnfoldLinearModelContinuousTime"
-		# load and generate a simulated Unfold Design
-		data,evts = UnfoldSim.predef_eeg(;)
-		data = reshape(data,1,size(data)...)
-		f  = @formula 0~1+condition+continuous
-		basis = firbasis([0,0.5],100)
-		# generate ModelStruct
-		return fit(UnfoldModel,Dict(Any=>(f,basis)) ,evts,data)
-		
-	elseif example == "TopoPlots.jl"
-		data,chanlocs = TopoPlots.example_data();
-		df = DataFrame(estimate=Float64[],time=Float64[],channel=Int64[],coefname=String[],topoPositions=[],se=Float64[],pval=Float64[])
-		pos = TopoPlots.points2mat(chanlocs)
-		for ch = 1:size(data,1)
-			for t = 1:size(data,2)
-				append!(df,DataFrame(estimate=data[ch,t,1],se=data[ch,t,2],pval=data[ch,t,3],time=t,channel=ch,coefname="A",topoPositions=(pos[1,ch],pos[2,ch])))
-				
-				
-			end
-		end
-		df.time = range(-0.3,0.5,step=1/500)[Int.(df.time)]
-		return df,chanlocs
-	else
-		error("unknown example data")
-	end
+            m = fit(
+                UnfoldModel,
+                (Dict(Any => (f, range(0, length = size(data, 2), step = 1 / 100)))),
+                evts,
+                data,
+            )
+            d = coeftable(m)
+            d.channel .= ch
+            cAll = append!(cAll, d)
+        end
+        return cAll
+
+    elseif example == "UnfoldLinearModelContinuousTime"
+        # load and generate a simulated Unfold Design
+        data, evts = UnfoldSim.predef_eeg(;)
+        data = reshape(data, 1, size(data)...)
+        f = @formula 0 ~ 1 + condition + continuous
+        basis = firbasis([0, 0.5], 100)
+        # generate ModelStruct
+        return fit(UnfoldModel, Dict(Any => (f, basis)), evts, data)
+
+    elseif example == "TopoPlots.jl"
+        data, chanlocs = TopoPlots.example_data()
+        df = DataFrame(
+            estimate = Float64[],
+            time = Float64[],
+            channel = Int64[],
+            coefname = String[],
+            topo_positions = [],
+            se = Float64[],
+            pval = Float64[],
+        )
+        pos = TopoPlots.points2mat(chanlocs)
+        for ch = 1:size(data, 1)
+            for t = 1:size(data, 2)
+                append!(
+                    df,
+                    DataFrame(
+                        estimate = data[ch, t, 1],
+                        se = data[ch, t, 2],
+                        pval = data[ch, t, 3],
+                        time = t,
+                        channel = ch,
+                        coefname = "A",
+                        topo_positions = (pos[1, ch], pos[2, ch]),
+                    ),
+                )
+
+
+            end
+        end
+        df.time = range(-0.3, 0.5, step = 1 / 500)[Int.(df.time)]
+        return df, chanlocs
+    else
+        error("unknown example data")
+    end
 end
