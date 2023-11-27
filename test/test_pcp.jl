ccc4c2f8ca0bcacccf38eb016cd590130d2059c0
Vladimir Mikheev <dezem@bk.ru>
2023-11-27 13:52:45 +0000

    only formatting and docs

--- test/test_pcp.jl
+++ test/test_pcp.jl
@@ -1,25 +1,36 @@
 
 @testset "Figure, 3 channels, 1 condition" begin
     include("../docs/example_data.jl")
-    results_plot, positions = example_data();
-    plot_parallelcoordinates(results_plot; # this selects channel 5,3 & 2 
-        mapping = (color = :coefname, y = :estimate))
+    results_plot, positions = example_data()
+    plot_parallelcoordinates(
+        results_plot; # this selects channel 5,3 & 2 
+        mapping = (color = :coefname, y = :estimate),
+    )
 end
 
 
 @testset "GridPosition" begin
     uf_5chan = example_data("UnfoldLinearModelMultiChannel")
-    d_singletrial, _ = UnfoldSim.predef_eeg(; return_epoched=true)
+    d_singletrial, _ = UnfoldSim.predef_eeg(; return_epoched = true)
 
     f = Figure()
-    plot_parallelcoordinates(f[1, 1], uf_5chan; 
-        mapping=(; color=:coefname), layout=(; legend_position=:bottom));
+    plot_parallelcoordinates(
+        f[1, 1],
+        uf_5chan;
+        mapping = (; color = :coefname),
+        layout = (; legend_position = :bottom),
+    )
     f
 end
 
 @testset "Bending" begin
     # check that the points actually go through the provided points
-    f,b,c,d = UnfoldMakie.parallelplot(Figure(),[0 1 0 2 0 3 0 4.; -1 0 -2 0 -3 0 -4 0]',normalize=:no,bend=true);
+    f, b, c, d = UnfoldMakie.parallelplot(
+        Figure(),
+        [0 1 0 2 0 3 0 4.0; -1 0 -2 0 -3 0 -4 0]',
+        normalize = :no,
+        bend = true,
+    )
     f
-    
+
 end
