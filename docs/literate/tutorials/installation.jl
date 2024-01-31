e7ceb1aa3ed9b200ac56d0c81cbc1152e375d8e2
Vladimir Mikheev <dezem@bk.ru>
2024-01-31 12:16:50 +0000

    bug was in code chunk changing env - should not be evaluated

--- docs/literate/tutorials/installation.jl
+++ docs/literate/tutorials/installation.jl
@@ -15,7 +15,7 @@
 # First you type `]` into the Julia console to switch from `julia` to `(@VERSION) pkg`. 
 # Here you can generate a project by using the command: 
 
-Pkg.generate("FOLDER_PATH")
+# `generate "FOLDER_PATH"`
 
 
 # Note that the specific folder in which you want to generate the project does not already exist.
@@ -25,24 +25,25 @@ Pkg.generate("FOLDER_PATH")
 # Before you can add the necessary modules to use UnfoldMakie you have to activate your project in the `(@VERSION) pkg` environment. 
 # The command is: 
 
-Pkg.activate("FOLDER_PATH")
+# `activate "FOLDER_PATH"`
 
 # ## Install the UnfoldMakie Module
 
 # When your project is activated you can add the module. 
 # The command is: 
 
-Pkg.add("UnfoldMakie")
+# `add UnfoldMakie`
 
 
-# ## Using the Project in a Notebook
+# ## Using the Project in a Pluto Notebook
 
 # In case you want to use this generated project in a notebook (e.g. [Pluto](https://www.juliapackages.com/p/pluto) or [Jupyter](https://ipython.org/notebook.html)), you can activate this in the notebook in the following manner:
 
+#=
 begin
     using Pkg
     Pkg.activate("FOLDER_PATH")
     Pkg.resolve()
-end
-
+end 
+=#
 # Use slash `/` for the folder path. 
