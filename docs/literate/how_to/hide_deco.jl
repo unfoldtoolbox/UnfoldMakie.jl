8b7f5820ea7725326fa8bca19f89743dae355de8
Vladimir Mikheev <dezem@bk.ru>
2024-02-01 11:30:50 +0000

    test 2

--- docs/literate/how_to/hide_deco.jl
+++ docs/literate/how_to/hide_deco.jl
@@ -12,15 +12,48 @@ Instead, these functions can be called by setting variables with `layout = (; hi
 Since these values reflect the input to the function, we can use an empty tuple to remove all decorations and spines, respectively
 
 And using `hidespines = (:r, :t)` will remove the top and right borders.
-
-For more information on the input of these functions refer to the [Makie dokumentation on Axis.](https://makie.juliaplots.org/v0.15.2/examples/layoutables/axis/#hiding_axis_spines_and_decorations)
-
-Since some plots hide features by default, the hiding can be reverted by setting the variables to `nothing`
 =#
 
 using TopoPlots
 using UnfoldMakie
 using CairoMakie
+
+include("../docs/example_data.jl")
+data, pos = example_data("TopoPlots.jl")
+
+f = Figure()
+plot_butterfly!(
+    f[1, 1],
+    data;
+    positions = pos,
+    topomarkersize = 10,
+    topoheigth = 0.4,
+    topowidth = 0.4,
+)
+plot_butterfly!(
+    f[2, 1],
+    data;
+    positions = pos,
+    topomarkersize = 10,
+    topoheigth = 0.4,
+    topowidth = 0.4,
+    layout = (; hidedecorations = (:label => true, :ticks => true, :ticklabels => true)),
+)
+for (label, layout) in zip(["with decorations", "without"], [f[1, 1], f[2, 1]])
+    Label(
+        layout[1, 1, TopLeft()],
+        label,
+        fontsize = 26,
+        font = :bold,
+        padding = (0, -250, 25, 0),
+        halign = :left,
+    )
+end
+f
+
+#=
+Since some plots hide features by default, the hiding can be reverted by setting the variables to `nothing`
+=#
 
 data, positions = TopoPlots.example_data()
 plot_topoplot(
@@ -28,3 +61,5 @@ plot_topoplot(
     positions = positions,
     layout = (; hidespines = nothing, hidedecorations = nothing),
 )
+
+# For more information on the input of these functions refer to the [Makie dokumentation on Axis.](https://makie.juliaplots.org/v0.15.2/examples/layoutables/axis/#hiding_axis_spines_and_decorations)
