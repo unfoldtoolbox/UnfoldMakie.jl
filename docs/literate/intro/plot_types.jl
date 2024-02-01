7ff640bdd07a691c4162c5d8e754998e80a53702
Vladimir Mikheev <dezem@bk.ru>
2024-02-01 10:15:18 +0000

    image addresses

--- docs/literate/intro/plot_types.jl
+++ docs/literate/intro/plot_types.jl
@@ -17,7 +17,7 @@ Possible dimensions:
 - Trials/subjects
 =#
 
-# <img src="../../src/assets/slicing.jpg" width="128"/>
+# <img src="../../../src/assets/slicing.jpg" width="128"/>
 
 #=
 Each way of ERP presentation is a choice of dimensions.
@@ -27,6 +27,6 @@ Hard to show meaningfully more than 3 dimensions.
 # # Plot types
 
 # Each plot type can represent several dimensions. Here we represented 8 plot types.
-# <img src="../../src/assets/dimensions.jpg" width="128"/>
+# <img src="../../../src/assets/dimensions.jpg" width="128"/>
 
 # If you want to know more about how we come up with these plot types, please read the paper [The Arte of Brainwaves](https://www.biorxiv.org/content/10.1101/2023.12.20.572507v2.full)
