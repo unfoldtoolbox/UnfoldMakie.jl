1ad856f9d3be2befb610ac6fda38abab60809812
Vladimir Mikheev <dezem@bk.ru>
2024-02-12 10:22:15 +0000

    bug3

--- src/docstring_template.jl
+++ src/docstring_template.jl
@@ -51,7 +51,7 @@ function _docstring(cfg_symb::Symbol)
 
     return """## Shared plot configuration options
         The shared plot options can be used as follows:
-        `type = (; key=value, ...))` - for example `plot_x(...; layout = (; show_legend=true, legend_position=:right))`. 
+        `type = (; key = value, ...))` - for example `plot_x(...; layout = (; show_legend = true, legend_position = :right))`. 
         Multiple defaults will be cycled until match.
 
         $(out)
