7aabfeaa08c2401e68e6dc965703ecf4c10e4bc9
Vladimir Mikheev <dezem@bk.ru>
2024-03-11 16:28:06 +0000

    stupid bug because of AOG

--- test/test_channelimage.jl
+++ test/test_channelimage.jl
@@ -1,13 +1,13 @@
-data, pos = TopoPlots.example_data()
-data = data[:, :, 1]
+dat, pos = TopoPlots.example_data()
+dat = dat[:, :, 1]
 pos = pos[1:30]
 
 @testset "channel image basic" begin
-    plot_channelimage(data, pos, raw_ch_names;)
+    plot_channelimage(dat, pos, raw_ch_names;)
 end
 
 @testset "channel image with Figure" begin
     f = Figure()
-    plot_channelimage!(f, data, pos, raw_ch_names;)
+    plot_channelimage!(f, dat, pos, raw_ch_names;)
 
 end
