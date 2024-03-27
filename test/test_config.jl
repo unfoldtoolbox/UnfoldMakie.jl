731504a48079cb92cc98354cdbe02f230b8c63f6
Vladimir Mikheev <dezem@bk.ru>
2024-03-27 13:36:23 +0000

    bug 3

--- test/test_config.jl
+++ test/test_config.jl
@@ -3,7 +3,7 @@
     UnfoldMakie.config_kwargs!(cfg; visual = (; bla = :blub))
     @test cfg.visual.bla == :blub
 
-    # now test that you cannot forget the ; - that is, cant forget to specify a NamedTuple
+    # What if you forget `;` - that is, forget to specify a `NamedTuple`.
     @test_throws AssertionError UnfoldMakie.config_kwargs!(cfg; visual = (bla = :blub))
 
 end
