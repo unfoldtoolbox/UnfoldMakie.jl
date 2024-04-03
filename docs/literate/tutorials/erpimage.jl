fa7c6eb9506c35263c9c775ca1a9e9742af14819
Vladimir Mikheev <dezem@bk.ru>
2024-04-03 10:12:49 +0000

    stupid line breaks

--- docs/literate/tutorials/erpimage.jl
+++ docs/literate/tutorials/erpimage.jl
@@ -30,8 +30,10 @@ plot_erpimage(data)
 # Generate the data and specify the necessary sorting parameter. 
 
 #=
-- `sortvalues::Vector{Int64} = false`\\ 
-    Parameter over which plot will be sorted. Using `sortperm()` of Base Julia.\\ 
+- `sortvalues::Vector{Int64} = false`
+
+    Parameter over which plot will be sorted. Using `sortperm()` of Base Julia.
+    
     `sortperm()` computes a permutation of the array's indices that puts the array in sorted order.
 =#
 
@@ -60,10 +62,14 @@ f
 # Since ERP images use a `Matrix` as an input, the library does not need any informations about the mapping.
 
 #=
-- `erpblur::Number = 10`\\
-    Number indicating how much blur is applied to the image. \\
-    Gaussian blur of the `ImageFiltering` module is used.\\
-- `meanplot::bool = false`\\
+- `erpblur::Number = 10`
+
+    Number indicating how much blur is applied to the image. 
+    
+    Gaussian blur of the `ImageFiltering` module is used.
+    
+- `meanplot::bool = false`
+
     Indicating whether the plot should add a line plot below the ERP image, showing the mean of the data.
 =#
 
