d68e01c1bc4b1aee950806fe16b230185c61c5e3
Vladimir Mikheev <dezem@bk.ru>
2024-04-22 17:27:26 +0000

    new version

--- ext/UnfoldMakiePyMNEExt.jl
+++ ext/UnfoldMakiePyMNEExt.jl
@@ -6,14 +6,14 @@ using UnfoldMakie
 """
     to_positions(raw::PyMNE.Py; kwargs...)
 
-Calls `make_eeg_layout` from MNE-Python with optional kwargs.
+Generates EEG positions. Calls `make_eeg_layout` from MNE-Python with optional kwargs.
 **Return Value:** `Vector{Point2{Float64}`.
     """
 function UnfoldMakie.to_positions(raw::PyMNE.Py; kwargs...)
     layout_from_raw = PyMNE.channels.make_eeg_layout(raw.info; kwargs...).pos
     positions = pyconvert(Array, layout_from_raw)[:, 1:2]
 
-    points = map(GeometryBasics.Point{2,Float64}, eachrow(positions))
+    points = map(GeometryBasics.Point{2, Float64}, eachrow(positions))
     return points
 end
 end
