8faaba04ec27d91818d760cf53f95148b0defccb
Vladimir Mikheev <dezem@bk.ru>
2024-04-22 17:33:02 +0000

    format

--- ext/UnfoldMakiePyMNEExt.jl
+++ ext/UnfoldMakiePyMNEExt.jl
@@ -13,7 +13,7 @@ function UnfoldMakie.to_positions(raw::PyMNE.Py; kwargs...)
     layout_from_raw = PyMNE.channels.make_eeg_layout(raw.info; kwargs...).pos
     positions = pyconvert(Array, layout_from_raw)[:, 1:2]
 
-    points = map(GeometryBasics.Point{2, Float64}, eachrow(positions))
+    points = map(GeometryBasics.Point{2,Float64}, eachrow(positions))
     return points
 end
 end
