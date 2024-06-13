f6b2af13f214631545c094c46fce6320e3e5f029
Vladimir Mikheev <dezem@bk.ru>
2024-06-13 14:10:08 +0000

    code principles (draft)

--- docs/literate/intro/code_principles.jl
+++ docs/literate/intro/code_principles.jl
@@ -1,1 +1,10 @@
-#TBD
+# # Code principles
+#= Here we will write about principles which we developed through our publication.
+
+- Code should be clear and concise
+- Variables inside the code should have meaningful names
+- Every function exposed to the user should have documentation that specifies all parameters, types, input and output arguments.
+- Most people will not look at the defaults, so it is very important to nudge users to show important details with a picture or text.
+- Function naming should be based on some theory and naming conventions.
+- You should avoid functions longer 50 lines 
+=#
