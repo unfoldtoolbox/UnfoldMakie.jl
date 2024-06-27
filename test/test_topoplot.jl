ec43f82bd8613a1634c87eec45a7a1ee379e3039
Vladimir Mikheev <dezem@bk.ru>
2024-06-27 13:23:06 +0000

    tests are not passed

--- test/test_topoplot.jl
+++ test/test_topoplot.jl
@@ -4,21 +4,30 @@ dat, positions = TopoPlots.example_data()
     plot_topoplot(dat[:, 50, 1]; positions = positions, axis = (; title = "topoplot"))
 end
 
-@testset "topoplot without legend" begin
+@testset "topoplot: no legend" begin
     plot_topoplot(dat[:, 50, 1]; positions = positions, layout = (; show_legend = false))
 end
 
-@testset "topoplot with xlabel" begin
+@testset "topoplot: xlabel" begin
     plot_topoplot(dat[:, 50, 1]; positions = positions, axis = (; xlabel = "[50 ms]"))
 end
 
-@testset "topoplot with GridLayout" begin
+@testset "topoplot: GridLayout" begin
     f = Figure()
     plot_topoplot!(f[1, 1], dat[:, 150, 1]; positions = positions)
     f
 end
 
-@testset "topoplot with labels" begin
+@testset "topoplot: labels" begin
     labels = ["s$i" for i = 1:size(dat[:, 150, 1], 1)]
     plot_topoplot(dat[:, 150, 1]; positions = positions, labels = labels)
+end
+
+@testset "topoplot: AbstractMatrix" begin
+    plot_topoplot(zeros(1:128))
+end
+
+@testset "topoplot: ViewArray" begin
+    d = Dict(:a => 11, :b => 12, :c => 13)
+    plot_topoplot(view(d, [:a, :c])) 
 end
