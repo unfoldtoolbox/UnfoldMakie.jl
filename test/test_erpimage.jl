69431d1e3c236c80c09577deb22f437da0abb6e4
Vladimir Mikheev <dezem@bk.ru>
2024-07-05 12:04:30 +0000

    issue 192

--- test/test_erpimage.jl
+++ test/test_erpimage.jl
@@ -77,6 +77,7 @@ end
     )
 end
 
+
 @testset "ERP image with show_sortval" begin
     dat_e, evts_e, times = example_data("sort_data")
     plot_erpimage(times, dat_e; sortvalues = evts_e.Δlatency, show_sortval = true)
