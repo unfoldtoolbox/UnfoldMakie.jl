86e8263be849c643987ece7aea2a3fd8958ea968
Vladimir Mikheev <dezem@bk.ru>
2024-08-08 16:04:32 +0000

    bug2

--- test/test_complexplots.jl
+++ test/test_complexplots.jl
@@ -69,7 +69,7 @@
 
     dat_e, evts, times = example_data("sort_data")
     plot_erpimage!(gf, times, dat_e; sortvalues = evts.Δlatency)
-    plot_channelimage!(gg, data[:, :, 1], positions[1:30], raw_ch_names;)
+    plot_channelimage!(gg, data[1:30, :, 1], positions[1:30], raw_ch_names;)
     r1, positions = example_data()
     r2 = deepcopy(r1)
     r2.coefname .= "B" # create a second category
