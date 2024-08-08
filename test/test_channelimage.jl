371357d7effc6e5cebc59d1f970036e55f614005
Vladimir Mikheev <dezem@bk.ru>
2024-08-08 15:41:33 +0000

    bug

--- test/test_channelimage.jl
+++ test/test_channelimage.jl
@@ -4,16 +4,15 @@ dat = dat[:, :, 1]
 pos = pos[1:30]
 
 df, pos2 = example_data("TopoPlots.jl")
-Matrix(df2)
-#df, pos = example_data("TopoPlots.jl")
+
 
 @testset "Channel image: 3 arguments, data as Matrix" begin
-    plot_channelimage(dat, pos, raw_ch_names;)
+    plot_channelimage(dat[1:30, :], pos, raw_ch_names;)
 end
 
 @testset "Channel image: 4 arguments, data as Matrix" begin
     f = Figure()
-    plot_channelimage!(f, dat, pos, raw_ch_names;)
+    plot_channelimage!(f, dat[1:30, :], pos, raw_ch_names;)
 
 end
 
@@ -27,7 +26,7 @@ end
 
 @testset "Channel image: error of unequal length of pos and ch_names" begin
     err1 = nothing
-    t() = error(plot_channelimage(dat, pos[1:10], raw_ch_names;))
+    t() = error(plot_channelimage(dat[1:30, :], pos[1:10], raw_ch_names;))
 
     try
         t()
@@ -37,7 +36,7 @@ end
 
 @testset "Channel image: sorting by y" begin
     plot_channelimage(
-        dat,
+        dat[1:30, :],
         pos,
         raw_ch_names;
         sorting_variables = [:y],
@@ -47,7 +46,7 @@ end
 
 @testset "Channel image: sorting by ch_names" begin
     plot_channelimage(
-        dat,
+        dat[1:30, :],
         pos,
         raw_ch_names;
         sorting_variables = [:ch_names],
@@ -57,7 +56,8 @@ end
 
 @testset "Channel image: error of unequal sorting_variables and sorting_reverse" begin
     err1 = nothing
-    t() = error(plot_channelimage(dat, pos, raw_ch_names; sorting_variables = [:y]))
+    t() =
+        error(plot_channelimage(dat[1:30, :], pos, raw_ch_names; sorting_variables = [:y]))
     try
         t()
     catch err1
@@ -66,8 +66,7 @@ end
 
 @testset "Channel image: error of unequal data and sorting_reverse" begin
     err1 = nothing
-    t() =
-        error(plot_channelimage(dat[:, 1:100], pos, raw_ch_names; sorting_variables = [:y]))
+    t() = error(plot_channelimage(dat, pos, raw_ch_names; sorting_variables = [:y]))
     try
         t()
     catch err1
