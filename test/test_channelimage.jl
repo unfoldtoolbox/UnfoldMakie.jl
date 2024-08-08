6128d128695ffaa407b72ab6936ccc7e9a6a0c52
Vladimir Mikheev <dezem@bk.ru>
2024-08-08 15:23:36 +0000

    data input and option for sorting methods for channel image

--- test/test_channelimage.jl
+++ test/test_channelimage.jl
@@ -3,6 +3,8 @@ dat, pos = TopoPlots.example_data()
 dat = dat[:, :, 1]
 pos = pos[1:30]
 
+df, pos2 = example_data("TopoPlots.jl")
+Matrix(df2)
 #df, pos = example_data("TopoPlots.jl")
 
 @testset "Channel image: 3 arguments, data as Matrix" begin
@@ -15,14 +17,57 @@ end
 
 end
 
-#= @testset "Channel image: 3 arguments, data as DataFrame" begin
-    plot_channelimage(df, pos, raw_ch_names;)
-end =#
+@testset "Channel image: 3 arguments, data as DataFrame" begin
+    f = Figure(size = (400, 800))
+    array = [string(i) for i = 1:64]
+    df2 = unstack(df[:, [:estimate, :time, :channel]], :channel, :time, :estimate)
+    select!(df2, Not(:channel))
+    plot_channelimage!(f, df2, pos2, array;)
+end
 
 @testset "Channel image: error of unequal length of pos and ch_names" begin
     err1 = nothing
     t() = error(plot_channelimage(dat, pos[1:10], raw_ch_names;))
 
+    try
+        t()
+    catch err1
+    end
+end
+
+@testset "Channel image: sorting by y" begin
+    plot_channelimage(
+        dat,
+        pos,
+        raw_ch_names;
+        sorting_variables = [:y],
+        sorting_reverse = [:true],
+    )
+end
+
+@testset "Channel image: sorting by ch_names" begin
+    plot_channelimage(
+        dat,
+        pos,
+        raw_ch_names;
+        sorting_variables = [:ch_names],
+        sorting_reverse = [:true],
+    )
+end
+
+@testset "Channel image: error of unequal sorting_variables and sorting_reverse" begin
+    err1 = nothing
+    t() = error(plot_channelimage(dat, pos, raw_ch_names; sorting_variables = [:y]))
+    try
+        t()
+    catch err1
+    end
+end
+
+@testset "Channel image: error of unequal data and sorting_reverse" begin
+    err1 = nothing
+    t() =
+        error(plot_channelimage(dat[:, 1:100], pos, raw_ch_names; sorting_variables = [:y]))
     try
         t()
     catch err1
