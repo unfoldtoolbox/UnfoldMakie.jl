59c8e249dc00141c3be704f10997137aff7feb4d
Vladimir Mikheev <dezem@bk.ru>
2024-10-02 09:13:20 +0000

    benchmarktools

--- docs/literate/intro/speed.jl
+++ docs/literate/intro/speed.jl
@@ -3,6 +3,7 @@ using UnfoldMakie
 using TopoPlots
 using PyMNE
 using PythonPlot
+using BenchmarkTools
 
 # Data input 
 dat, positions = TopoPlots.example_data()
@@ -10,13 +11,14 @@ df = UnfoldMakie.eeg_array_to_dataframe(dat[:, :, 1], string.(1:length(positions
 
 # Topoplots
 
-@time plot_topoplot(dat[:, 320, 1]; positions = positions)
+@benchmark plot_topoplot(dat[:, 320, 1]; positions = positions)
 
+#
 posmat = collect(reduce(hcat, [[p[1], p[2]] for p in positions])')
 pypos = Py(posmat).to_numpy()
 pydat = Py(dat[:, 320, 1])
 
-@time begin
+@benchmark begin
     f = PythonPlot.figure()
     PyMNE.viz.plot_topomap(
         pydat,
@@ -31,7 +33,7 @@ pydat = Py(dat[:, 320, 1])
 end
 
 # Topoplot series
-@time begin
+@benchmark begin
     plot_topoplotseries(
         df;
         bin_num = 20,
@@ -40,13 +42,11 @@ end
     )
 end
 
-
-
-
+#
 easycap_montage = PyMNE.channels.make_standard_montage("standard_1020")
 ch_names = pyconvert(Vector{String}, easycap_montage.ch_names)[1:64]
 info = PyMNE.create_info(PyList(ch_names), ch_types = "eeg", sfreq = 1)
 info.set_montage(easycap_montage)
 simulated_epochs = PyMNE.EvokedArray(Py(dat[:, :, 1]), info)
 
-@time simulated_epochs.plot_topomap(1:20)
+@benchmark simulated_epochs.plot_topomap(1:20)
