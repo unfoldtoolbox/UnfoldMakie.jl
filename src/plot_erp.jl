3e59c29af62665c1bee1e025939d5716624e97cc
Vladimir Mikheev <dezem@bk.ru>
2025-02-04 13:10:41 +0100

    important check

--- src/plot_erp.jl
+++ src/plot_erp.jl
@@ -127,9 +127,10 @@ function plot_erp!(
     end
 
     if !haskey(config.mapping, :color)
-        config_kwargs!(config; visual = (; colormap = nothing, color = :black))
-
-        @warn """By default we used `black` color for lines. If you need something else, please specify `config.visual.color`."""
+        if !haskey(config.visual, :color)
+            config_kwargs!(config; visual = (; colormap = nothing, color = :black))
+            @warn """By default we used `black` color for lines. If you need something else, please specify `config.visual.color`."""
+        end
         is_categorical = true
     else
 
