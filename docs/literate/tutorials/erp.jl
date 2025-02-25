b5e1d403c42458775bc2c638f3be7f3c73a2526e
Vladimir Mikheev <33777074+vladdez@users.noreply.github.com>
2025-02-25 12:42:46 +0000

    Update docs/literate/tutorials/erp.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/literate/tutorials/erp.jl
+++ docs/literate/tutorials/erp.jl
@@ -38,7 +38,11 @@ res_effects = effects(Dict(:continuous => -5:0.5:5), m);
 plot_erp(results)
 
 # To change legend title use `mapping.color`:
-plot_erp(results, mapping = (; color = :coefname => "Conditions"), axis = (; xlabel = "Time [s]"))
+plot_erp(
+    results,
+    mapping = (; color = :coefname => "Conditions"),
+    axis = (; xlabel = "Time [s]"),
+)
 
 # # Additional features
 
