6e7631d33c541d6083004f8b282333853cc7aaec
Vladimir Mikheev <dezem@bk.ru>
2025-11-03 16:19:36 +0100

    videos

---
+++ docs/literate/public.jl
@@ -0,0 +1,12 @@
+# # Overview & Demo Video
+# Here we will share some links telling more about UnfoldMakie.jl:
+
+# ```@raw html
+# <div align="center">
+# <iframe width="560" height="315" 
+# src="https://www.youtube.com/embed/KMmZ9yR1v-k?si=BZ2U9ZEdQJzcGq4D" 
+# title="YouTube video player" frameborder="0" allow="accelerometer; 
+# autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; 
+# web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+# </div>
+# ```
