c7ceacd79f32001684586de4dd15f83be5c794d4
Vladimir Mikheev <dezem@bk.ru>
2026-03-04 13:31:24 +0100

    bugfix

--- docs/literate/how_to/montage.jl
+++ docs/literate/how_to/montage.jl
@@ -8,25 +8,9 @@ for (name, desc) in builtin_montages
     println("$(name): $(desc)")
 end
 
-begin
-    f = Figure(size = (800, 500))
-    for (i, (title, montage)) in enumerate(
-        [("Biosemi", biosemi_montage), ("10-20", montage_1020), ("10-05", standard_1005), ("brainproducts-128", brainproducts-RNP-BA-128)], 
-    )
-        labels = pyconvert(Vector{String}, montage.ch_names)
-        ch_pos = pyconvert(Dict{String,Any}, montage.get_positions()["ch_pos"])
-        pos3d = hcat([ch_pos[l] for l in labels]...)
-        pos2 = to_positions(pos3d)
-
-        scatter(f[1, i], pos2, axis = (title = title,))
-        text!(f[1, i], labels, position = Point2f.(pos2),
-            align = (:center, :center), fontsize = 16)
-    end
-    f
-end
 
 begin
-    f = Figure(size = (1200, 500))
+    f = Figure(size = (1200, 800))
 
     montages = [
         ("Biosemi", "biosemi64"),
@@ -36,6 +20,8 @@ begin
     ]
 
     for (i, (title, mname)) in enumerate(montages)
+        row = (i - 1) ÷ 2 + 1
+        col = (i - 1) % 2 + 1
         montage = PyMNE.channels.make_standard_montage(mname)
 
         labels = pyconvert(Vector{String}, montage.ch_names)
@@ -43,9 +29,14 @@ begin
         pos3d = hcat([ch_pos[l] for l in labels]...)
         pos2 = to_positions(pos3d)
 
-        scatter(f[1, i], pos2, axis = (title = title,))
-        text!(f[1, i], labels, position = Point2f.(pos2),
-            align = (:center, :center), fontsize = 16)
+        scatter(f[row, col], pos2, axis = (title = title,))
+        text!(
+            f[row, col],
+            labels,
+            position = Point2f.(pos2),
+            align = (:center, :center),
+            fontsize = 16,
+        )
     end
 
     f
