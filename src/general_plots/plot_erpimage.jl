e33c0d8b2df7faa1539faac9fd1222aacb7bbc6a
Vladimir Mikheev <33777074+vladdez@users.noreply.github.com>
2026-04-22 16:08:31 +0200

    Safer terming
    Agree with suggestions
    
    Co-authored-by: Benedikt Ehinger <science@benediktehinger.de>

--- src/general_plots/plot_erpimage.jl
+++ src/general_plots/plot_erpimage.jl
@@ -49,10 +49,10 @@ $(_docstring(:erpimage))
 function plot_erpimage(data::AbstractArray{<:Real,3}; kwargs...)
     error(
         "plot_erpimage received 3D array of size $(size(data)) " *
-        "(channels, time, trials). " *
+        "likely (channels, time, trials). " *
         "Select or fix a single channel first. " *
-        "Format should follow `plot_erpimage(data[x, :, :])`, " *
-        " where x is a constant."
+        "E.g. via  `plot_erpimage(data[x, :, :])`, " *
+        " where x is a integer."
     )
 end
 
